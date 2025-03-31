b20b6e1ded3dba538253f07c49581a54751da3d1
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-03-31 15:18:06 -0400

    bug 9

---
+++ bugs/bugs09.js
@@ -0,0 +1,33 @@
+//Find the bug. This function should display the full name of the user
+
+function getFullName(user) {
+    return user.firstName + " " + user.lastname;
+  }
+  
+  const user = {
+    firstName: "Alice",
+    lastName: "Johnson"
+  };
+  
+  console.log('getFullName:',getFullName(user));
+
+  //Find the bug
+
+  function createUser() {
+    const user = {
+      name: "Tina",
+      contact: {
+        email: "tina@example.com"
+      },
+      getContactInfo: () => {
+        return `${this.name} can be reached at ${this.contact?.email}`;
+      }
+    };
+  
+    return user;
+  }
+  
+  const users = createUser();
+  console.log(users.getContactInfo());
+  
+  
