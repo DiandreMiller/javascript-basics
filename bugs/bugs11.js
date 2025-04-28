10dc4899c8691b7bef4bed3411f6a638029c7315
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-04-28 14:33:05 -0400

    class bug

--- bugs/bugs11.js
+++ bugs/bugs11.js
@@ -15,3 +15,19 @@ class Dog extends Animal {
 
 const dog = new Dog('Buddy', 'Golden Retriever');
 console.log(dog.name, dog.breed);
+
+
+//Find the bug
+
+class Calculator {
+    static add(a, b) {
+        return a + b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+}
+
+const calc = new Calculator();
+console.log(calc.add(2, 3));
