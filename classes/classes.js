4c0a398c6dfef6d50d40a550e18d773808b81469
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-04-28 02:51:13 -0400

    classes

---
+++ classes/classes.js
@@ -0,0 +1,18 @@
+// 1.	Create and Inherit a Method
+// •	Create a vehicle object with a move() method.
+// •	Create a car object that inherits from vehicle.
+// •	Call move() from car.
+
+
+
+// 2. Class with Instance Method
+// 	•	Create a Book class that takes title and author as arguments in its constructor.
+// 	•	Add a describe() method inside the class that logs the book’s title and author.
+// 	•	Create two instances of Book and call describe() on each.
+
+
+
+// 3.	Static Method
+// •	Create a class Calculator with a static method multiply(a, b) that returns the product.
+// •	Call Calculator.multiply(4, 5).
+
