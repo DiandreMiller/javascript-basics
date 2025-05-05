d0cac776041b29c9b543bd7c880541ead32f1901
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-05-05 04:58:09 -0400

    extendingClasses.js added

---
+++ oop/classes/extendingClasses.js
@@ -0,0 +1,77 @@
+// 1. Add a .first() method to all arrays
+
+// Instruction:
+// Add a method called first to all arrays that returns the first element 
+// of the array.
+
+
+
+
+// 2. Add a .last() method to all arrays
+
+// Instruction:
+// Create a method that returns the last item in an array.
+
+
+
+
+// 3. Add a .reverseString() method to all strings
+
+// Instruction:
+// Create a method that reverses any string.
+
+
+
+
+// 4. Add a .isEven() method to all numbers
+
+// Instruction:
+// Create a method that returns true if the number is even, otherwise false.
+
+
+
+
+// 5. Add a .capitalize() method to all strings
+
+// Instruction:
+// Add a method that returns the string with the first character capitalized.
+
+
+
+
+// 6. Add a .sum() method to all arrays
+
+// Instruction:
+// Create a method that returns the sum of all numbers in the array.
+
+
+
+
+// 7. Add a .countVowels() method to strings
+
+// Instruction:
+// Return the number of vowels in the string.
+
+
+
+
+// 8. Add a .hasKey() method to all objects
+
+// Instruction:
+// Return true if the object has the specified key, false otherwise.
+
+
+
+
+// 9. Add a .double() method to numbers
+
+// Instruction:
+// Return the number multiplied by 2.
+
+
+
+
+// 10. Add a .chunk(size) method to arrays
+
+// Instruction:
+// Split the array into smaller arrays of a given size.
