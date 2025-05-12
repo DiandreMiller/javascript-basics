27f9ae30bb18ab9e7b74672e3c4abfe0445dc33f
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-05-12 19:03:59 -0400

    bugs14.js

---
+++ bugs/bugs14.js
@@ -0,0 +1,15 @@
+// Find the bug
+
+class Timer {
+    #seconds = 0;
+  
+    start() {
+      setInterval(function () {
+        this.#seconds++;
+        console.log(this.#seconds);
+      }, 1000);
+    }
+  }
+  
+  const timer = new Timer();
+  timer.start(); // Throws TypeError: Cannot read private member #seconds from an object whose class did not declare it
