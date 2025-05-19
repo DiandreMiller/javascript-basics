f5f5b559cec71cd310501dbc7a3f29a4e7653b6a
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-05-19 04:12:39 -0400

    extending classes test

---
+++ test/extendingClassesTest.js
@@ -0,0 +1,22 @@
+//Create a method called "removeSpaces" that remotes leading and trailing
+//spaces in string. Add it to the String.prototype
+
+
+// Create a method called "removeDuplicates" that removes duplicate elements 
+// from an array. Add it to the Array.prototype.
+
+
+// Create a method called "average" that returns the average of all 
+// numbers in an array. Add it to the Array.prototype.
+
+
+
+// Create a method called "last" that mimics "pop". 
+// It should return a new array containing the original array's length 
+// and its last element. Add it to the Array.prototype.
+
+
+
+
+// Create a method called "addToEnd" that mimics "push". 
+//Return the updated array. Add it to the Array.prototype.
