8950cecf59ffec34ff37993d104b58c9417977a8
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-06-01 16:09:29 -0400

    method chaining

--- oop/this/methodChaining.js
+++ oop/this/methodChaining.js
@@ -2,3 +2,8 @@
 
 // Build a class to manage a todo list with methods like .addTask(task), 
 // .removeTask(index), .clear(), and .printTasks(), all supporting chaining.
+
+// 🔁 2. Design a StringBuilder class
+
+// This class should allow chaining of string manipulations like .append(str), 
+// .prepend(str), .toUpperCase(), and .print().
