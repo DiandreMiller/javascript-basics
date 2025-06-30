276667a095a75184ee043e75fde706a3a2d0db46
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-06-30 14:59:45 -0400

    linklist.js

--- oop/linkedList.js
+++ oop/linkedList.js
@@ -1,1 +1,17 @@
-//For linked Lists problems
+// 1. Implement a Singly Linked List
+
+// Description:
+// Create a singly linked list with the following operations:
+// 	•	append(data)
+// 	•	prepend(data)
+// 	•	deleteWithValue(value)
+// 	•   deleteAllWithValue(value)
+
+// 	•	printList()
+
+// list.append(1);
+// list.append(2);
+// list.prepend(0);
+// list.deleteWithValue(2);
+// list.printList(); // Output: 0 -> 1
+
